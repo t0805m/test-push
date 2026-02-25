098b7c5a993ee377fc07b3b4700488a7fd654124
NZ <email@example.com>
2026-02-25 13:48:46 +0300

    wip

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func main() {
 	}
 
 	// Эндпоинт для отправки silent push на один device token
-	http.HandleFunc("/send", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/send-silent", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
 			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			return
